d8774464692ee50b4a53c1c9ee9fb6e46a3cfe05
chs186372 <36875805@qq.com>
2013-07-12 10:08:50 +0800

    combox修改

--- web/js/busi/user/login-user-his.js
+++ web/js/busi/user/login-user-his.js
@@ -27,7 +27,7 @@ $("#edit_app", page).click(function() {
     var rowData = $(this).getRow();
     if (rowData) {
         sessionStorage.login_person_count = JSON.stringify({username:rowData.username,from_node:'4'});
-        $.pdialog.open("page/user/login-person-time.html", 'login-person-time', rowData.username, {"width": 800, "height": 510});
+        $.pdialog.open("page/user/login-person-time.html", 'login-person-time', 'app.js客户端登录详细', {"width": 800, "height": 510});
     }
 });
 
@@ -35,6 +35,6 @@ $("#edit_pc", page).click(function() {
     var rowData = $(this).getRow();
     if (rowData) {
         sessionStorage.login_person_count = JSON.stringify({username:rowData.username,from_node:'2'});
-        $.pdialog.open("page/user/login-person-time.html", 'login-person-time', rowData.username, {"width": 800, "height": 510});
+        $.pdialog.open("page/user/login-person-time.html", 'login-person-time', '网站登录详细', {"width": 800, "height": 510});
     }
 });
